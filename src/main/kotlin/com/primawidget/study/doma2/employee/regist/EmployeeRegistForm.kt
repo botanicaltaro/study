ef4ab7e2d29ccbb022ab52d7d275e58166f66705
botanical-taro <rainbowbamboo1102@kcn.jp>
2020-08-16 03:52:20 +0900

    employee

---
+++ src/main/kotlin/com/primawidget/study/doma2/employee/regist/EmployeeRegistForm.kt
@@ -0,0 +1,12 @@
+package com.primawidget.study.doma2.todolist
+
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+class MemoUpdateForm {
+    @NotBlank
+    @Size(max = 20)
+    var content:String? = null
+
+    var done:Boolean = false
+}
